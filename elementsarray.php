a7e4b6cb1e0c7a4177c8f5bb9447df2fb517568f
kirubaja <152260859+kirubaja@users.noreply.github.com>
2023-11-30 13:39:48 +0530

    Add files via upload

---
+++ elementsarray.php
@@ -0,0 +1,14 @@
+<!DOCTYPE html>
+<html>
+<body>
+<?php
+
+$arr = [18, 25, 46, 33, 88, 99];
+echo "Elements in the array:\n";
+foreach ($arr as $element) {
+    echo $element . "\n";
+}
+
+?>
+</body>
+</html>
